7ad13d00edece0ee3b8fb8e55acf75c782b8e41b
coditumlabs <coditumlabs@10.144.4.15>
2017-01-30 17:53:15 -0500

    jhygiugouygoug

--- src/PieToast/Cards.java
+++ src/PieToast/Cards.java
@@ -1,6 +1,7 @@
 package PieToast;
 
 import java.lang.reflect.Array;
+import java.util.Random;
 
 public class Cards {
 public String süt;
@@ -25,9 +26,22 @@ if(value>1&&value<11){
 	System.out.println("King of "+süt);
 }
 }
+public Cards RandomCard(){
+	Random rand=new Random();
+	int randomcard=rand.nextInt(13)+1;
+	int randomsüt=rand.nextInt(3);
+	String randomsüt2="";
+	if (randomsüt==0){
+		return
+	}
+	
+	Cards card1=new Cards(randomsüt2, randomcard);
+	
+}
+//test
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Cards testcard=new Cards("Justin Bieber's Foot",13);
+		Cards testcard=new Cards("Cheese",13);
 		testcard.print();
 		Cards[] deck=new Cards[3];
 			
---
+++ src/PieToast/Functions.java
@@ -0,0 +1,30 @@
+package PieToast;
+
+public class Functions 
+{
+	public static void message()
+	{
+		System.out.println("Dioxys is a mutant alien sausage!");
+	}
+	public static int five()
+	{
+		return 5;
+	}
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+//
+		message();
+		System.out.println(multiplï(2,10)+"<--number stuf");
+		
+	}
+public static int dubble(int sumthing)
+{
+	return 2*sumthing;
+}
+public static int multiplï(int a, int b)
+{
+	return a*b;
+}
+}
