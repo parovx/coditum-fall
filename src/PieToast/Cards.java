90b35756c96d44e8e5ab647e363cce27a40eb882
coditumlabs <coditumlabs@10.144.4.25>
2017-02-13 18:01:20 -0500

    b

--- src/PieToast/Cards.java
+++ src/PieToast/Cards.java
@@ -5,31 +5,33 @@ import java.util.Random;
 
 public class Cards {
 public String süt;
-//ace=1 jack=11 queen=12 king=13
+//2=0 3=1 4=2 5=3 6=4 7=5 8=6 9=7 10=8 jack=9 queen=10 king=11 ace=12 joker=13
 public int value;
 
 public Cards(String suit,int valyü){
 	süt=suit;
 	value=valyü;
 }
-// ace=1 jack=11 queen=12 king=13
+// ace=12 jack=9 queen=10 king=11 joker=13
 public void print(){
-if(value>1&&value<11){
-	System.out.println(value+" of "+süt);
-	}else if(value==1){
+if(value<9){
+	System.out.println(value+2+" of "+süt);
+	}else if(value==12){
 		System.out.println("Ace of "+süt);
-}else if(value==11){
+}else if(value==9){
 	System.out.println("Jack of "+süt);
-}else if(value==12){
+}else if(value==10){
 	System.out.println("Queen of "+süt);
-}else{
+}else if(value==11){
 	System.out.println("King of "+süt);
+}else{
+	System.out.println("(^_^)JOKER(^_^)");
 }
 }
 //Hearts=0 Spades=1 Diamonds=2 Clubs=3
 public static Cards RandomCard(){
 	Random rand=new Random();
-	int randomcard=rand.nextInt(13)+1;
+	int randomcard=rand.nextInt(14);
 	int randomsüt=rand.nextInt(3);
 	String randomsüt2="";
 	if (randomsüt==0){
@@ -48,12 +50,12 @@ public static Cards RandomCard(){
 }
 //test
 	public static void main(String[] args) {
-		Cards card2=new Cards("hello",13);
-		Cards card1=new Cards("hello",139889);
+		Cards card2=new Cards("hello",138778);
+		Cards card1=new Cards("hello",189778);
 		System.out.print(card1.greaterthan(card2));
 	}
 	public boolean greaterthan(Cards othercard){
-		return this.value>othercard.value;
+		return this.value>=othercard.value;
 	}
 
 }
--- src/PieToast/WAR.java
+++ src/PieToast/WAR.java
@@ -1,5 +1,7 @@
 package PieToast;
 
+
+
 public class WAR {
 
 	public static void main(String[] args)	{
@@ -7,7 +9,35 @@ public class WAR {
 		//two empty decks for the cards you/opponent win
 		// compare top cards, winner gets both
 		//when tied, put down 3 cards. Then take a card from the top. Better one wins all cards on the field
-		Cards[] deck= new Cards[54];
+
+		Cards[] deck= new Cards[27];
+		Cards[] deck2= new Cards[27];
+		deck[0]=Cards.RandomCard();
+		for (int cardx=0; cardx<27; cardx++){
+			deck[cardx]=Cards.RandomCard();
+			System.out.println("Player 1");
+			System.out.println("Card "+cardx);
+			deck[cardx].print();
+			System.out.println("__________________");
+			deck2[cardx]=Cards.RandomCard();
+			System.out.println("Player 2");
+			System.out.println("Card "+cardx);
+			deck2[cardx].print();
+			System.out.println("___________________");
+		}
+		int top=0;
+		while(top<27){
+			if(deck2[top].greaterthan(deck[top])){
+				System.out.println("Player 2 (^_^)WINS(^_^)!");
+			}else if(deck[top].greaterthan(deck2[top])){
+				System.out.println("Player 1 (^_^)WINS(^_^)!");
+			}else{
+				System.out.println("tie");
+			}top++;
+		}
 	}
 
 }
+
+
+
