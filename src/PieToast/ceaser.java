344d9d844b0e93011351bdcf7caa636ee63ea796
coditumlabs <coditumlabs@10.144.4.38>
2017-03-13 17:57:13 -0400

    xwqefrwtrgethtybntg hillo

--- src/PieToast/ceaser.java
+++ src/PieToast/ceaser.java
@@ -3,26 +3,29 @@ package PieToast;
 import java.util.Scanner;
 
 public class ceaser {
+	//ASetBttq i A>cM i
 //a=z b=a c=b d=c e=d f=e g=f h=g i=h j=i k=j l=k m=l n=m o=n p=o q=p r=q s=r t=s u=t v=u w=v x=w y=x z=y
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scan= new Scanner(System.in);
 		String input=scan.next();
 		String plaintext=input;
+		String ciphertext="";
 		char letter;
-		/*String ciphertext="";
-		for(int i=0; i<plaintext.length(); i++){
-			letter=plaintext.charAt(i);
-			ciphertext+="";
-			//learn about switch statements
-		 System.out.println(ciphertext);*/
+		char key=scan.next().charAt(0);
 		
-		//(char) ascii_input
+		int shift=key-97;
 		for(int i=0; i<input.length(); i++) {
 			char c = input.charAt(i);
-			System.out.println(c);
+			int nv=c;
+			nv+=shift;
+			if(nv>=122){
+			nv-=26;}
+			c=(char)nv;
+			System.out.print(c);
+			
 		}
 		
 	}
 }
-//}
+
