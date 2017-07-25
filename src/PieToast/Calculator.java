e583a7ba7d2cff17a4f0f68e1e3caa260d56359f
SummerTech <SummerTech@10.144.4.19>
2017-07-25 13:30:09 -0400

    shgtrgh

--- src/PieToast/Calculator.java
+++ src/PieToast/Calculator.java
@@ -27,14 +27,18 @@ public class Calculator {
 				int1/=int2;
 				System.out.println(int1);
 			}else {
-				char char1=rand.toString();
-				String Strange=
-				int wudmg=rand.nextInt();
+				String charc1=( ("") || ("a") || ("b") || ("c") || ("d") || ("e") || ("f") || ("g") || ("h") || ("i") || ("j") || ("k") || ("l") || ("m") || ("n") || ("o") || ("p") || ("q") || ("r") || ("s") || ("t") || ("u") || ("v") || ("w") || ("x") || ("y") || ("z"));
+				String charc2=(("a") || ("b") || ("c") || ("d") || ("e") || ("f") || ("g") || ("h") || ("i") || ("j") || ("k") || ("l") || ("m") || ("n") || ("o") || ("p") || ("q") || ("r") || ("s") || ("t") || ("u") || ("v") || ("w") || ("x") || ("y") || ("z"));
+				String charc3=(("a") || ("b") || ("c") || ("d") || ("e") || ("f") || ("g") || ("h") || ("i") || ("j") || ("k") || ("l") || ("m") || ("n") || ("o") || ("p") || ("q") || ("r") || ("s") || ("t") || ("u") || ("v") || ("w") || ("x") || ("y") || ("z"));
+				String charc4=(("a") || ("b") || ("c") || ("d") || ("e") || ("f") || ("g") || ("h") || ("i") || ("j") || ("k") || ("l") || ("m") || ("n") || ("o") || ("p") || ("q") || ("r") || ("s") || ("t") || ("u") || ("v") || ("w") || ("x") || ("y") || ("z"));
+				String charc5=(("a") || ("b") || ("c") || ("d") || ("e") || ("f") || ("g") || ("h") || ("i") || ("j") || ("k") || ("l") || ("m") || ("n") || ("o") || ("p") || ("q") || ("r") || ("s") || ("t") || ("u") || ("v") || ("w") || ("x") || ("y") || ("z"));
+				//String Strange=();
+				int wudmg = rand.nextInt();
 				int wuhhlt=rand.nextInt();
 				int wuspe=rand.nextInt();
-				String Wuname=
+				String Wuname=(""+charc1+""+charc2+""+charc3+""+charc4+""+charc5+"");
 				Enemy lalala=new Enemy(wudmg, Wuname, wuspe, wuhhlt );
-				System.out.println(lalala.getName()+" says:"+Strange+"!");
+				//System.out.println(lalala.getName()+" says:"+Strange+"!");
 			}
 		}
 	}
