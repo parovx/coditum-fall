a43adf457d077c9d05938d3a5615a15c53768371
coditum <coditum@10.144.4.5>
2017-10-21 13:44:57 -0400

    Thanks for buying bjshuff, the best free app for blackjack shuffling. There are also in-app purchases, such as bjshuff, our only in-app purchase. The features for this free app without the purchases, are: Being able to purchase our in-app purchases. App rating: 1/5 *'s. That's .2!

--- src/PieToast/cardgame.java
+++ src/PieToast/cardgame.java
@@ -1,15 +1,16 @@
 package PieToast;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class cardgame {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ArrayList blackjacklist=new ArrayList();
-		ArrayList blackjackplayer=new ArrayList();
-		ArrayList blackjackdealer=new ArrayList();
-		ArrayList blackjackdeck=new ArrayList();
+		ArrayList<Integer> blackjacklist=new ArrayList<Integer>();
+		ArrayList<Integer> blackjackplayer=new ArrayList<Integer>();
+		ArrayList<Integer> blackjackdealer=new ArrayList<Integer>();
+		ArrayList<Integer> blackjackdeck=new ArrayList<Integer>();
 		for(int i=0; i<4; i++){
 			blackjackdeck.add(10);
 			blackjackdeck.add(10);
@@ -18,16 +19,29 @@ public class cardgame {
 				blackjackdeck.add(i,j);
 			}
 		}
+		Random rand=new Random();
+		int randbjshuffto=rand.nextInt(1234567890);
+		System.out.println("Shuffling...");
+		for(int shuffleblackjack=0; shuffleblackjack<randbjshuffto; shuffleblackjack++){
+			int bjshuff1=(int)(Math.random() * blackjackdeck.size());
+			int bjshuff2=(int)(Math.random()*blackjackdeck.size());
+			
+			int bjtemp1=blackjackdeck.get(bjshuff1);
+			int bjtemp2=blackjackdeck.get(bjshuff2);
+			
+			blackjackdeck.set(bjshuff1, bjtemp2);
+			blackjackdeck.set(bjshuff2, bjtemp1);
+		}
 		//test
 			System.out.println(blackjackdeck);
 			System.out.println(blackjackplayer);
 			System.out.println(blackjackdealer);
 		//real
-			
+			//blackjackdeck.
 	//test
-		System.out.println(blackjackdeck);
-		System.out.println(blackjackplayer);
-		System.out.println(blackjackdealer);
+		//System.out.println(blackjackdeck);
+		//System.out.println(blackjackplayer);
+		//System.out.println(blackjackdealer);
 	}
 
 }
