9ff77e9811b190baf99ccf5704e6f73591a84f35
SummerTech <SummerTech@10.144.4.71>
2018-07-09 10:00:31 -0400

    jyujnh j6hb fvfgrthyuj5yt533

---
+++ src/PieToast/madlibs.java
@@ -0,0 +1,14 @@
+package PieToast;
+
+import java.util.Scanner;
+
+public class madlibs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Give me a noun.");
+		System.out.println("Give me an adjective.");
+	}
+
+}
---
+++ src/PieToast/therandomclass.java
@@ -0,0 +1,21 @@
+package PieToast;
+
+import java.util.Scanner;
+
+public class therandomclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi");
+		int eger;
+		eger=1;
+		System.out.println(eger);
+		String string;
+		string="hello";
+		System.out.println(string);
+		Scanner scan=new Scanner(System.in);
+		String egg00;
+		egg00=scan.next();
+		System.out.println(egg00);
+	}
+}
