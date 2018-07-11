e5436e634a50edc81243a13f38f71d943d34b6ab
temp <temp>
2018-07-11 09:59:10 -0400

    vgyigftuftftm nm8 m

--- src/PieToast/Connect2.java
+++ src/PieToast/Connect2.java
@@ -21,5 +21,75 @@ public class Connect2 {
 			}
 			System.out.println("");
 		}
+		boolean win=false;
+		while (win==false) {
+			System.out.println(playerrname+" choose a column.");
+			int rchoice=scan.nextInt();
+			for(int w=5; w>=0; w--) {
+				if(q[w][rchoice].equals("-")) {
+					q[w][rchoice]="r";
+					break;
+				}
+				if(w==0 && !q[w][rchoice].equals("-")){
+					System.out.println("This column is full.");
+					boolean full=true;
+					while(full==true) {
+						System.out.println(playerrname+" choose a column.");
+						rchoice=scan.nextInt();
+						if(w==0 && !q[w][rchoice].equals("-")){
+							System.out.println("This column is full.");
+						}else {
+							for(int i=5; i>=0; i--) {
+								if(q[i][rchoice].equals("-")) {
+									q[i][rchoice]="r";
+									break;
+								}
+							}
+							full=false;
+						}
+					}
+				}
+			}
+			for(int e=0; e<6; e++) {
+				for(int r=0; r<7; r++){
+					System.out.print(q[e][r]+" ");
+				}
+				System.out.println("");
+			}
+
+			System.out.println(playeryname+" choose a column.");
+			int ychoice=scan.nextInt();
+			for(int w=5; w>=0; w--) {
+				if(q[w][ychoice].equals("-")) {
+					q[w][ychoice]="y";
+					break;
+				}
+				if(w==0 && !q[w][ychoice].equals("-")){
+					System.out.println("This column is full.");
+					boolean full=true;
+					while(full==true) {
+						System.out.println(playeryname+" choose a column.");
+						ychoice=scan.nextInt();
+						if(w==0 && !q[w][ychoice].equals("-")){
+							System.out.println("This column is full.");
+						}else {
+							for(int i=5; i>=0; i--) {
+								if(q[i][ychoice].equals("-")) {
+									q[i][ychoice]="y";
+									break;
+								}
+							}
+							full=false;
+						}
+					}
+				}
+			}
+			for(int e=0; e<6; e++) {
+				for(int r=0; r<7; r++){
+					System.out.print(q[e][r]+" ");
+				}
+				System.out.println("");
+			}
+		}
 	}
 }
