85ff2aa770ac84685260fd862845caa0b42fb3a5
temp <temp>
2018-07-12 11:01:01 -0400

    tytrtrhhrth

--- src/PieToast/Battleship2.java
+++ src/PieToast/Battleship2.java
@@ -6,6 +6,7 @@ public class Battleship2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
 		String[][] b=new String[10][10];
 		for(int a=0; a<10; a++) {
 			for(int c=0; c<10; c++) {
@@ -13,14 +14,27 @@ public class Battleship2 {
 			}
 		}
 		printBoard(b);
-		System.out.println("Choose the row of the center of your ship that takes up three spaces.");
-		Scanner scan=new Scanner(System.in);
-		int player1rowchoice=scan.nextInt();
-		System.out.println("Choose the column of the center of your ship that takes up three spaces.");
-		int player1columnchoice=scan.nextInt();
-		System.out.println("Would you like your ship to be going up or down?( Press 1 for up, press 2 for down.)");
-		b[player1rowchoice][player1columnchoice]="o";
-		b[player1rowchoice+1][player1columnchoice]="o";
+		System.out.println("Would you like your ship to be going up and down or left and right?( Press 1 for up and down, press 2 for left and right.)");
+		int p1upordown=scan.nextInt();
+		System.out.println("Choose the row of the center part of your ship that takes up three spaces.");
+		int p1rchoice=scan.nextInt();
+		p1rchoice--;
+		System.out.println("Choose the column center part of your ship that takes up three spaces.");
+		int p1cchoice=scan.nextInt();
+		p1cchoice--;
+		if(p1upordown==1) {
+				b[p1rchoice][p1cchoice]="o";
+				b[p1rchoice+1][p1cchoice]="o";
+				b[p1rchoice-1][p1cchoice]="o";
+			}else if(p1upordown==2) {
+				b[p1rchoice][p1cchoice]="o";
+				b[p1rchoice][p1cchoice+1]="o";
+				b[p1rchoice][p1cchoice-1]="o";
+			}else {
+				while(1<2) {
+					System.out.println("E");
+				}
+			}
 		printBoard(b);
 	}
 	public static void printBoard(String[][] b) {
