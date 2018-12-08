220f4d8878458dc7a720f03ae096722ac9cbc9d6
coditum <coditum@10.144.4.32>
2018-12-08 13:41:01 -0500

    ityughj

--- src/PieToast/Uno.java
+++ src/PieToast/Uno.java
@@ -4,7 +4,11 @@ public class Uno {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Cards[] deck= new Cards[7];
+		Cards[] deck2= new Cards[7];
+		Cards[] deck3= new Cards[94];
+		deck[0]=Cards.RandomCard();
+		System.out.println();
 	}
 
 }
--- src/PieToast/UnoCards.java
+++ src/PieToast/UnoCards.java
@@ -1,14 +1,55 @@
 package PieToast;
 
+import java.util.Random;
+
 public class UnoCards {
 	public String colour;
 	public int value;
 	public UnoCards(String coloor, int valoo){
-		
+		colour=coloor;
+		value=valoo;
 	}
+	public void print(){
+		if(value<10){
+			System.out.println(colour+" "+value);
+		}else if(value>9 && value<19){
+			int temp=value-9;
+			System.out.println(colour+" "+temp);
+		}else if(value==19){
+			System.out.println(colour+" 0");
+		}else if(value==20 || value==21){
+			System.out.println(colour+" skip turn");
+		}else if(value==22 || value==23){
+			System.out.println(colour+" reverse");
+		}else if(value==24 || value==25){
+			System.out.println(colour+" draw two");
+		}else if(value==26){
+			System.out.println("WILD CARD");
+		}else{
+			System.out.println("DRAW FOUR");
+		}
+	}
+	public static UnoCards RandomUnoCards(){
+		Random rand=new Random();
+		int randomcard=rand.nextInt(27);
+		int randomcolor=rand.nextInt(3);
+		String randomcolor2="";
+		if (randomcolor==0){
+			randomcolor2="Red";
+		}else if (randomcolor==1){
+			randomcolor2="Yellow";
+		}else if (randomcolor==2){
+			randomcolor2="Green";
+		}else{
+			randomcolor2="Blue";
+		}
+		UnoCards carda=new UnoCards(randomcolor2, randomcard);
+		return carda;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println();
 	}
 
 }
