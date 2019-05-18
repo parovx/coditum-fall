9e556b824163ee138de1e4e3333c6b64e93ed033
coditum <coditum@10.144.4.9>
2019-05-18 11:41:18 -0400

    griouy3

---
+++ src/PieToast/Bucket.java
@@ -0,0 +1,22 @@
+package PieToast;
+
+public class Bucket {
+public int kee;
+public String value;
+public Bucket(){
+	this.kee=kee;
+	this.value=value;
+}
+public int getkee(){
+	return kee;
+}
+public void setkee(int setterofkee){
+	kee=(setterofkee);
+}
+public String getvalue(){
+	return value;
+}
+public void setvalue(String setterofvalue){
+	value.equals(setterofvalue);
+}
+}
--- src/PieToast/HashMap.java
+++ src/PieToast/HashMap.java
@@ -1,10 +1,30 @@
 package PieToast;
 
 public class HashMap {
+	private Bucket map[];
+	public HashMap(int hmm){
+		map=new Bucket[hmm];
+	}
+	public HashMap(){
+		map=new Bucket[4];
+	}
+	public boolean torf(String value){
+		int check= hash(value);
+		Bucket tempBucket=map[check];
+		while(tempBucket!=null&&!(tempBucket.getkee()==(hash(value)))){
+			
+		}
+	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+
+	public int hash(String data){
+		int sum=0;
+		for(int a=0; a<data.length(); a++){
+			int b=data.charAt(a);
+			sum=sum+b;
+		}
+		return sum/Integer.MAX_VALUE;
 	}
 
 }
