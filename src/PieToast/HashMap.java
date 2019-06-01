be47c8734fe219e1f948bf321ac4d8d81a41e4bf
coditum <coditum@10.144.4.9>
2019-06-01 11:42:38 -0400

    ergij

--- src/PieToast/HashMap.java
+++ src/PieToast/HashMap.java
@@ -8,15 +8,30 @@ public class HashMap {
 	public HashMap(){
 		map=new Bucket[4];
 	}
-	public boolean torf(String value){
+	/*public boolean torf(String value){
 		int check= hash(value);
 		Bucket tempBucket=map[check];
 		while(tempBucket!=null&&!(tempBucket.getkee()==(hash(value)))){
-			
+
 		}
+	}*/
+
+	public void adder(String ake, String duckit){
+		String first="a";
+		String second="b";
+		String third="c";
+		String fourth="d";
+		if(ake.equals(1)){
+			duckit=first;
+		}else if(ake.equals(2)){
+			duckit=second;
+		}else if(ake.equals(3)){
+			duckit=third;
+		}else{
+			duckit=fourth;
+		}
+		//map [1]=;
 	}
-
-
 
 	public int hash(String data){
 		int sum=0;
