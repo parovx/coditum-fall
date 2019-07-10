f9b2268239b9d695875346e01e7f38e60133d2dd
stuser <stusr@summertech.net>
2019-07-10 13:30:13 -0400

    houn uyb bhr7tygbkhjuhgvbuyj

--- src/PieToast/Connect2.java
+++ src/PieToast/Connect2.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class Connect2 {
 	public static void main(String[] args) {
 		Scanner scan=new Scanner(System.in);
-		System.out.println("Player red, choose your one-word name.");
-		String playerrname=scan.next();
-		System.out.println("Player yellow, choose your one-word name.");
-		String playeryname=scan.next();
+		System.out.println("Player red, choose your name.");
+		String playerrname=scan.nextLine();
+		System.out.println("Player yellow, choose your name.");
+		String playeryname=scan.nextLine();
 		String[][] q=new String[6][7];
 		for(int e=0; e<6; e++) {
 			for(int r=0; r<7; r++) {
--- src/PieToast/maze.java
+++ src/PieToast/maze.java
@@ -1,18 +1,30 @@
 package PieToast;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class maze {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
-		Scanner  scan=new Scanner(new File("\\Users\\user\\git\\coditum-fall\\src\\PieToast\\themaze"));
-		int isthisthemaze[][]=new int[scan.nextInt()][scan.nextInt()];
+		Scanner scan = new Scanner(new File("C:\\Users\\User\\git\\coditum-fall\\coditum-fall\\coditum-fall\\coditum-fall\\src\\PieToast\\themaze"));
+		String isthisthemaze[][]=new String[scan.nextInt()][scan.nextInt()];
 		String line=scan.nextLine();
-		for(int thismightbethemazelength=0; thismightbethemazelength<isthisthemaze.length; thismightbethemazelength++) {
-			isthisthemaze[thismightbethemazelength][thismightbethemazelength]=line.charAt(thismightbethemazelength);
+		System.out.println(line);
+		for(int thismightbethemazelength=0; thismightbethemazelength<(isthisthemaze.length); thismightbethemazelength++) {
+			line=scan.nextLine();
+			System.out.println(line);
+			isthisthemaze[thismightbethemazelength][thismightbethemazelength]=line;
+		}
+		printMaze(isthisthemaze);
+	}
+	public static void printMaze(String[][] isthisthemaze) {
+		for(int e=0; e<7; e++) {
+			for(int r=0; r<7; r++){
+				System.out.print(isthisthemaze[e][r]);
+			}
+			System.out.println("");
 		}
 	}
-
 }
