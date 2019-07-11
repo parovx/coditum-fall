2636e1da9d53bf8b802ea17ea0c8e0787e778dcb
stuser <stusr@summertech.net>
2019-07-11 09:58:17 -0400

    6wjsr

--- src/PieToast/maze.java
+++ src/PieToast/maze.java
@@ -13,9 +13,14 @@ public class maze {
 		String line=scan.nextLine();
 		System.out.println(line);
 		for(int thismightbethemazelength=0; thismightbethemazelength<(isthisthemaze.length); thismightbethemazelength++) {
+			String[] whatifthelinewasanarray = isthisthemaze[thismightbethemazelength];
 			line=scan.nextLine();
-			System.out.println(line);
-			isthisthemaze[thismightbethemazelength][thismightbethemazelength]=line;
+			for(int turnthelineintobrackets=0; turnthelineintobrackets<(isthisthemaze[0].length); turnthelineintobrackets++) {
+				String nottheline=line;
+				whatifthelinewasanarray[turnthelineintobrackets]=Character.toString(nottheline.charAt(turnthelineintobrackets));
+			}
+
+			isthisthemaze[thismightbethemazelength]=whatifthelinewasanarray;
 		}
 		printMaze(isthisthemaze);
 	}
