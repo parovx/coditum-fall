efc1398dbc5809da1dfcfc2b43c297e2b9d015ae
bingo <aaa@aaa.aaa>
2019-07-29 11:11:23 -0400

    WEEWOOWEEWOO

---
+++ src/PieToast/heaps.java
@@ -0,0 +1,11 @@
+package PieToast;
+
+public class heaps {
+	public int []heap;
+	int currentsize;
+	int maxsize;
+	public heaps(int maxsize) {
+		this.currentsize=0;
+		this.maxsize=heap.length;
+	}
+}
