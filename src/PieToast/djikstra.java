e2b75b88849a94bc996b6ec983396408c19ac79e
bingo <aaa@aaa.aaa>
2019-08-01 11:14:50 -0400

    y4rgeazwwawa

--- src/PieToast/djikstra.java
+++ src/PieToast/djikstra.java
@@ -10,11 +10,26 @@ public class djikstra {
 	public void addedge(int sauce, int destination, int cost) {
 		dam.addedge(sauce, destination, cost);		
 	}
-	public void getminimumvertex(boolean[] arrey, int []aray) {
+	public int getminimumvertex(boolean[] arrey, int []aray) {
 		int minkey=Integer.MAX_VALUE;
 		int monkey=-1;
 		for(int check=0; check<vertices; check++) {
-			if(aray[check]<minkey) {
+			if(aray[check]<minkey&& arrey[check]==false) {
+				minkey=aray[check];
+				monkey=check;
+			}
+		}
+		return monkey;
+	}
+	public int djikstracalc(int sauce) {
+		boolean[] airay= new boolean[vertices];
+		int[] urray=new int [vertices];
+		for(int chek=0; chek<vertices; chek++) {
+			urray[chek]=Integer.MAX_VALUE;
+		}
+		int minvertex=getminimumvertex(airay, urray);
+		for(int sheck=0; sheck<airay.length; sheck++) {
+			if(airay[sheck]<) {
 				
 			}
 		}
