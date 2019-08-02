ea3c189c3934b83605ee4fcb5858f0eb861e90ff
bingo <aaa@aaa.aaa>
2019-08-02 10:02:41 -0400

    tfgi tgvkl hlgc v ,

--- src/PieToast/adjacencymatrix.java
+++ src/PieToast/adjacencymatrix.java
@@ -40,7 +40,7 @@ public class adjacencymatrix {
 	}
 	public int getminimumvertex(boolean[] arrey, int []aray) {
 		int minkey=Integer.MAX_VALUE;
-		int monkey=-1;
+		int monkey=0;
 		for(int check=0; check<size; check++) {
 			if(aray[check]<minkey&& arrey[check]==false) {
 				minkey=aray[check];
@@ -49,26 +49,28 @@ public class adjacencymatrix {
 		}
 		return monkey;
 	}
-	public int djikstracalc(int sauce) {
-		boolean[] checc= new boolean[size];
+	public int dijkstracalc(int sauce) {
+		boolean[] checkingboolean= new boolean[size];
 		int[] distances=new int [size];
 		for(int chek=0; chek<size; chek++) {
 			distances[chek]=Integer.MAX_VALUE;
 		}
 		distances[sauce]=0;
-		for(int sheck=0; sheck<size; sheck++) {
-			int minvertex=getminimumvertex(checc, distances);
-			checc[minvertex]=true;
-			for(int shecc=0; shecc<size; shecc++) {
-				if(adjacencyarray[minvertex][shecc]>0) {
-					if(checc[shecc]==false && adjacencyarray[minvertex][sheck]!=Integer.MAX_VALUE) {
-						
+		for(int sheccloop1=0; sheccloop1<size; sheccloop1++) {
+			int minvertex=getminimumvertex(checkingboolean, distances);
+			checkingboolean[minvertex]=true;
+			for(int sheccloop2=0; sheccloop2<size; sheccloop2++) {
+				if(adjacencyarray[minvertex][sheccloop2]>0) {
+					if(checkingboolean[sheccloop2]==false && adjacencyarray[minvertex][sheccloop1]!=Integer.MAX_VALUE) {
+						int newminimum=distances[minvertex]+adjacencyarray[minvertex][sheccloop2];
+						if(distances[sheccloop2]<newminimum) {
+							distances[sheccloop2]=newminimum;
+						}
 					}
 				}
+
 			}
-			/*	if(urray[sheck]<minvertex) {
-				sheck=minvertex;
-			}*/
 		}
+		return distances[size-1];
 	}
 }
--- src/PieToast/iamtestingtheadjacencymatrix.java
+++ src/PieToast/iamtestingtheadjacencymatrix.java
@@ -7,21 +7,24 @@ public class iamtestingtheadjacencymatrix {
 		adjacencymatrix am=new adjacencymatrix(5);
 		am.addedge(0, 1, 1);
 		am.addedge(3, 4, 1);
-		am.addedge(2, 3, 1);
-		am.addedge(4, 3, 1);
-		am.addedge(1, 0, 1);
+		am.addedge(2, 3, 4);
+		am.addedge(4, 3, 3);
+		am.addedge(1, 0, 2);
 		am.addedge(3, 2, 1);
-		am.addedge(1, 0, 1);
+		am.addedge(1, 0, 2);
 		am.addedge(2, 3, 1);
-		am.addedge(3, 2, 1);
-		am.addedge(4, 2, 1);
+		am.addedge(3, 2, 4);
+		am.addedge(4, 2, 3);
+		am.addedge(1, 1, 2);
+		am.addedge(2, 1, 1);
+		am.addedge(3, 1, 3);
+		am.addedge(3, 3, 2);
+		am.addedge(4, 4, 4);
 		am.printmatrix();
 		System.out.println();
-		am.addcolumn(2);
-		am.addedge(5, 4, 1);
-		am.addedge(6, 5, 1);
-		am.addedge(5, 3, 1);
 		am.printmatrix();
+		System.out.println();
+		System.out.println(am.dijkstracalc(0));
 	}
 
 }
