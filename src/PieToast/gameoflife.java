a8f530bd98ae3a9e185a811aa1c6001ef5dee3e7
pasqu <pasqu@DESKTOP-0H403RP>
2020-04-10 18:02:40 -0400

    efsefagsezrgaer

--- src/PieToast/gameoflife.java
+++ src/PieToast/gameoflife.java
@@ -31,6 +31,7 @@ public class gameoflife {
 			}
 			printboard();
 		}
+		game(10, 0);
 	}
 	public static void printboard() {
 		for(int boardprinter=0; boardprinter<10; boardprinter++) {
@@ -45,12 +46,38 @@ public class gameoflife {
 	public static void game(int capacity, int gen) {
 		if(gen==capacity) {
 			return;
-		}
-		for(int killer=0; killer<10; killer++) {
-			for(int killer1=0; killer1<10; killer1++) {
-				if(board[killer][killer1]==1) {
-					//for(int circler=0; circler<)
+		}else{
+			int[][] tempboard=new int[10][10];
+			for(int boardtotemp1=0; boardtotemp1<10; boardtotemp1++) {
+				for(int boardtotemp2=0; boardtotemp2<10; boardtotemp2++) {
+					tempboard[boardtotemp1][boardtotemp2]=board[boardtotemp1][boardtotemp2];
 				}
+			}
+			for(int maker1=0; maker1<10; maker1++) {
+				for(int maker2=0; maker2<10; maker2++) {
+					int neighbors=0;
+					for(int circler1=-1; circler1<2; circler1++) {
+						for(int circler2=-1; circler2<2; circler2++) {
+							if((maker1+circler1>0 && maker1+circler1<10) && (maker2+circler2>0 && maker2+circler2<10) && board[maker1+circler1][maker2+circler2]==(1)) {
+								neighbors++;
+							}
+						}
+					}
+					if(board[maker1][maker2]==0) {
+						if(neighbors==3) {
+							tempboard[maker1][maker2]=1;
+						}
+					}else{
+						neighbors--;
+						if(neighbors<2 || neighbors>4) {
+							tempboard[maker1][maker2]=0;
+						}
+					}
+
+				}
+				board=tempboard;
+				printboard();
+				game(capacity, gen+1);
 			}
 		}
 	}
