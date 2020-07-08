246b69bc159f1b5b015408dfbceb8c787b030c55
pasqu <pasqu@DESKTOP-0H403RP>
2020-07-08 12:05:35 -0400

    aaaaaaaaaaaaaaaaaaaaaaaaaa

--- src/PieToast/mergesort.java
+++ src/PieToast/mergesort.java
@@ -6,40 +6,35 @@ public class mergesort {
 	static int size=4;
 	static int[] array = {1, 2, 3, 4};
 	public static void main(String[] agrs) {
-		split();
+		merge(array, );
 	}
 	public static void split() {
 		int split=size;
 		int splitcounter=0;
-		int[][] temparray=new int[size][];
-		int[]righthalf=new int[size/2];
+		int[]righthalf=new int[size];
 		while(split>2) {
 			split=split/2;
-			splitcounter++;
 		}
 		int[]smallarray=new int[split];
-		for(int middletemp=0; middletemp<splitcounter; middletemp++) {
-			for(int addtotemp=0; addtotemp<split; addtotemp++) {		
-				smallarray[addtotemp]=array[addtotemp];	
-			}
-			if(smallarray[0]>smallarray[1]) {
-				int templeft=smallarray[0];
-				smallarray[0]=smallarray[1];
-				smallarray[1]=templeft;
-			}
-			temparray[middletemp]=smallarray;
+		for(int addtotemp=0; addtotemp<split; addtotemp++) {		
+			smallarray[addtotemp]=array[addtotemp];
 		}
-		//merge(temparray);
+		if(smallarray[0]>smallarray[1]) {
+			int templeft=smallarray[0];
+			smallarray[0]=smallarray[1];
+			smallarray[1]=templeft;
+		}
+		merge(smallarray, righthalf);
 	}
 	public static void print(int[] printed) {
 		for(int printer=0; printer<printed.length; printer++) {
 			System.out.print(printed[printer]+", ");
 		}
 	}
-	/*public static int merge(int[][] thearray) {
+	public static void merge(int[] thearray, int[] anotherarray) {
 		int[] finalmerged=new int[size];
-		for(int x=0; x<size; x++) {
-			finalmerged[x]=thearray[x];
-		}
-	}*/
+			
+		array=finalmerged;
+		return;
+	}
 }
