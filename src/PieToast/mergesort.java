f20ef0a79be2450da5d070a51dd583cb3b9def8d
pasqu <pasqu@DESKTOP-0H403RP>
2020-07-08 10:00:31 -0400

    e ren rbip;a

--- src/PieToast/mergesort.java
+++ src/PieToast/mergesort.java
@@ -3,20 +3,21 @@ package PieToast;
 import java.lang.reflect.Array;
 
 public class mergesort {
-	static int size=5;
-	static int[] array = {1, 2, 3, 4, 5, 6};
+	static int size=4;
+	static int[] array = {1, 2, 3, 4};
 	public static void main(String[] agrs) {
 		split();
 	}
 	public static void split() {
 		int split=size;
 		int splitcounter=0;
-		while(split!=2 || split!=1) {
+		int[][] temparray=new int[size][];
+		int[]righthalf=new int[size/2];
+		while(split>2) {
 			split=split/2;
 			splitcounter++;
 		}
-		int[][] temparray=new int[size][];
-		int[] smallarray=new int[split];
+		int[]smallarray=new int[split];
 		for(int middletemp=0; middletemp<splitcounter; middletemp++) {
 			for(int addtotemp=0; addtotemp<split; addtotemp++) {		
 				smallarray[addtotemp]=array[addtotemp];	
@@ -28,17 +29,17 @@ public class mergesort {
 			}
 			temparray[middletemp]=smallarray;
 		}
-		merge(temparray);
+		//merge(temparray);
 	}
 	public static void print(int[] printed) {
 		for(int printer=0; printer<printed.length; printer++) {
 			System.out.print(printed[printer]+", ");
 		}
 	}
-	public static int merge(int[][] thearray) {
+	/*public static int merge(int[][] thearray) {
 		int[] finalmerged=new int[size];
 		for(int x=0; x<size; x++) {
 			finalmerged[x]=thearray[x];
 		}
-	}
+	}*/
 }
